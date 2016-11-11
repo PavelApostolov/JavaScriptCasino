bda2ef8e77e35822c3337b923f15392b55e607a2
Pavel Apostolov <pavel.apostolov@yahoo.com>
2016-11-11 16:37:01 +0200

    JavaScript Casino - Telerik Academy project

---
+++ TeamMotaroProject/scripts/common/systemjs-config.js
@@ -0,0 +1,31 @@
+SystemJS.config({
+    'transpiler': 'plugin-babel',
+    'map': {
+        'plugin-babel': '../lib/plugin-babel.js',
+        'systemjs-babel-build': '../lib/systemjs-babel-browser.js',
+        'jquery': '../lib/jquery-3.1.0.min.js',
+        'bootstrap': '../lib/bootstrap.min.js',
+        'firebase': '../lib/firebase.js',
+        'jqueryUI': '../lib/jquery-ui.min.js',
+        'main': '../scripts/common/main.js',
+        'validator': '../scripts/common/validator.js',
+        'users-authentication': '../scripts/common/users-authentication.js',
+        'user-controller': '../scripts/controllers/user-controllers.js',
+
+        'models': '../scripts/common/models.js',
+
+        'blackjack': '../scripts/blackjack/ui.js',
+        'blackjackEngine': '../scripts/blackjack/engine.js',
+        'blackjackModels': '../scripts/blackjack/models.js',
+
+        'roulette': '../scripts/roulette/ui.js',
+        'rouletteEngine': '../scripts/roulette/engine.js',
+        'rouletteModels': '../scripts/roulette/models.js',
+
+        'slotMachine': '../scripts/slot-machine/ui.js',
+        'slotMachineEngine': '../scripts/slot-machine/engine.js',
+        'slotMachineModels': '../scripts/slot-machine/models.js'
+    }
+});
+
+System.import('main');
